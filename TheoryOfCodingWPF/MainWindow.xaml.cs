e0454bef7d4061720dbb45613d0c74741f295579
Nikki <71009109+bilskla@users.noreply.github.com>
2021-05-05 16:59:57 +0300

    Decoding update
    Now text from right field can be decoded

--- TheoryOfCodingWPF/MainWindow.xaml.cs
+++ TheoryOfCodingWPF/MainWindow.xaml.cs
@@ -8,7 +8,13 @@ using System.Windows.Controls;
 using System.Windows.Documents;
 using Coding;
 using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Coding.Haffman;
+using Coding.Lz77.Resources;
+using Coding.RleAndBurrowsWheeler;
+using Coding.RleAndBurrowsWheeler.Resources;
 using Compress;
 using ComboBox = System.Windows.Controls.ComboBox;
 using MessageBox = System.Windows.MessageBox;
@@ -28,24 +34,25 @@ namespace TheoryOfCoding
 
         public string TypeOfCoding = "Haffmen";
         public static string InitialText = "";
+        private object _dictionary;
 
         private BaseCoder[] _coders = new BaseCoder[]
         {
-            new Coding.Haffman.Coder(), 
+            new Coding.Haffman.Coder(),
             new Coding.ShannonFano.Coder(),
-            new Coding.ArithmeticCoding.Coder(), 
+            new Coding.ArithmeticCoding.Coder(),
             new Coding.RleAndBurrowsWheeler.Coder(),
             new Coding.Lz77.Coder()
         };
 
-        private BaseDecoder[] _decoders = new BaseDecoder[]
-        {
-            new Coding.Haffman.Decoder(),
-            new Coding.ShannonFano.Decoder(),
-            new Coding.ArithmeticCoding.Decoder(),
-            new Coding.RleAndBurrowsWheeler.Decoder(),
-            new Coding.Lz77.Decoder()
-        };
+        //private BaseDecoder[] _decoders = new BaseDecoder[]
+        //{
+        //    new Coding.Haffman.Decoder(),
+        //    new Coding.ShannonFano.Decoder(),
+        //    new Coding.ArithmeticCoding.Decoder(),
+        //    new Coding.RleAndBurrowsWheeler.Decoder(),
+        //    new Coding.Lz77.Decoder()
+        //};
 
         private readonly Dictionary<string, int> _codes = new Dictionary<string, int>()
         {
@@ -56,12 +63,12 @@ namespace TheoryOfCoding
             { "LZ77", 4 }
         };
 
-        
+
 
         private void TypeOfCodingSelected(object sender, RoutedEventArgs e)
         {
-            ComboBox box = (ComboBox) sender;
-            ComboBoxItem selected = (ComboBoxItem) box.SelectedItem;
+            ComboBox box = (ComboBox)sender;
+            ComboBoxItem selected = (ComboBoxItem)box.SelectedItem;
             if (selected.Content != null)
             {
                 TextBlock tb = (TextBlock)selected.Content;
@@ -93,14 +100,15 @@ namespace TheoryOfCoding
 
         private void EncodeButton_Click(object sender, RoutedEventArgs e)
         {
-            InitialText = InitialAvalon.Text.TrimEnd('\r', '\n');
+            InitialText = InitialAvalon.Text;
             if (InitialText.Length == 0)
             {
-                MessageBox.Show("ПУСТО!");
+                MessageBox.Show("Text in left field is empty!", "Warning!", MessageBoxButton.OK);
                 return;
             }
             int codingIndex = _codes[TypeOfCoding];
             string encoded = _coders[codingIndex].Encode(InitialText);
+            _dictionary = _coders[codingIndex].GetCodes();
             double ratio;
             switch (codingIndex)
             {
@@ -112,8 +120,31 @@ namespace TheoryOfCoding
                     break;
             }
             CompRatio.Content = ratio;
+            //encoded = ReplaceEscapes(encoded);
             TransformedAvalon.Clear();
             TransformedAvalon.Text = encoded;
+        }
+
+        private string ReplaceEscapes(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '\n':
+                        result.Append("\\n");
+                        break;
+                    case '\r':
+                        result.Append("\\r");
+                        break;
+                    default:
+                        result.Append(input[i]);
+                        break;
+                }
+            }
+
+            return result.ToString();
         }
 
         private BigInteger ParseBigInteger(string input)
@@ -127,9 +158,79 @@ namespace TheoryOfCoding
             string[] nums = input.Split('^');
             return int.Parse(nums[1]);
         }
+
         private void DecodeButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("ВОТ БЛИН, ДЕКОДИРОВАНИЯ НЕТУ :(");
+            string EncodedText = TransformedAvalon.Text;
+            if (EncodedText.Length == 0)
+            {
+                MessageBox.Show("Text in right field is empty!", "Warning!", MessageBoxButton.OK);
+                return;
+            }
+
+            int codingIndex = _codes[TypeOfCoding];
+            string decoded = "";
+            switch (codingIndex)
+            {
+                case 0:
+                    Coding.Haffman.Decoder decoderHaffmen = new Coding.Haffman.Decoder();
+                    decoded = decoderHaffmen.Decode(EncodedText, (Dictionary<string, char>)_dictionary);
+                    break;
+                case 1:
+                    Coding.ShannonFano.Decoder decoderFanoShannon = new Coding.ShannonFano.Decoder();
+                    decoded = decoderFanoShannon.Decode(EncodedText, (Dictionary<string, char>)_dictionary);
+                    break;
+                case 2:
+                    Coding.ArithmeticCoding.Decoder decoderArythmetic = new Coding.ArithmeticCoding.Decoder();
+                    decoded = decoderArythmetic.Decode((Dictionary<char, long>)_dictionary,
+                        ParseBigInteger(EncodedText), ParsePower(EncodedText));
+                    break;
+                case 3:
+                    Coding.RleAndBurrowsWheeler.Decoder decoderBWT = new Coding.RleAndBurrowsWheeler.Decoder();
+                    decoded = decoderBWT.Decode(ParseTransformResult(EncodedText));
+                    break;
+                case 4:
+                    Coding.Lz77.Decoder decoderLZ = new Coding.Lz77.Decoder();
+                    decoded = decoderLZ.Decode(ParseNodes(EncodedText));
+                    break;
+
+            }
+            InitialAvalon.Clear();
+            InitialAvalon.Text = decoded;
+        }
+
+        private IEnumerable<Node> ParseNodes(string input)
+        {
+            List<Node> Result = new List<Node>();
+            string pattern = "(?<=\\()(\\d+), (\\d+), '(.+)(?=')";
+            Regex regex = new Regex(pattern);
+            MatchCollection collection = regex.Matches(input);
+            foreach (Match cum in collection)
+            {
+                int offset = Convert.ToInt32(cum.Groups[1].Value);
+                int length = Convert.ToInt32(cum.Groups[2].Value);
+                char nextBoliNet = cum.Groups[3].Value == "eof" ? '\0' : Convert.ToChar(cum.Groups[3].Value);
+                Result.Add(new Node(offset, length, nextBoliNet));
+            }
+
+            return Result;
+        }
+
+        private TransformResult ParseTransformResult(string input)
+        {
+            string pattern = "(?<=\\d)\\s";
+            Regex regex = new Regex(pattern);
+            string[] parts = regex.Split(input);
+            int position = Convert.ToInt32(parts[0]);
+            List<char> chars = new List<char>();
+            List<int> ints = new List<int>();
+            for (int i = 1; i < parts.Length; i++)
+            {
+                chars.Add(Convert.ToChar(parts[i][0]));
+                ints.Add(Convert.ToInt32(parts[i].Substring(1)));
+            }
+
+            return new TransformResult(position, chars, ints);
         }
     }
 }
